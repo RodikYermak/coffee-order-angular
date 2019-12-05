861408793074fbfb3f5ac37e7356cce7f379cf33
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-12-04 20:47:32 -0800

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { OrdersComponent } from "./orders/orders.component";
+import { OrderListComponent } from "./order-list/order-list.component";
+
+import { OrdersService } from "./shared/orders.service";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, OrdersComponent, OrderListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [OrdersService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCSqpjFZF2ELVFVDDT5Xj0tNT_F90g-ZF8",
+    authDomain: "coffee-order-17579.firebaseapp.com",
+    databaseURL: "https://coffee-order-17579.firebaseio.com",
+    projectId: "coffee-order-17579",
+    storageBucket: "coffee-order-17579.appspot.com",
+    messagingSenderId: "135440855370",
+    appId: "1:135440855370:web:392887a9d7f507990b91b3"
+  }
 };
 
 /*
